dc429fb965ee437c486da60df0272f0d4cc32037
Karlen Khachaturyan <karlen.khachaturyan@gmail.com>
2024-09-15 15:11:25 +0400

    FIRST INIT

---
+++ Intro to vue 3 (cdn)/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "Something",
+            description: "Some description",
+            img: './assets/images/warior-1.jpg',
+            url: "https://www.google.com/",
+            inStock: true,
+        }
+    }
+})
