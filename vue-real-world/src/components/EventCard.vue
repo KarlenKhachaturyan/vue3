42bca355a5dce511bc80c60538976532f9e47a59
Karlen Khachaturyan <karlen.khachaturyan@gmail.com>
2024-09-16 17:38:48 +0400

    added events card, removed the initial components

---
+++ vue-real-world/src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    default: () => {}
+  }
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{event.title}}</h2>
+    <span>@{{event.time}} on {{event.date}}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    transform: scale(1.01);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+}
+</style>
--- vue-real-world/src/views/HomeView.vue
+++ vue-real-world/src/views/HomeView.vue
@@ -1,9 +1,54 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from "@/components/EventCard.vue";
+import {ref} from 'vue'
+
+const events = ref([
+    {
+      id: 1,
+      category: 'a',
+      title: 'asdf',
+      description: 'asdf',
+      location: 'sadfas',
+      date: "Jan 28, 2022",
+      time: '12:00',
+      petsAllowed: false,
+      organizer: 'Admin'
+    },
+  {
+    id: 2,
+    category: 'a',
+    title: 'asdf',
+    description: 'asdf',
+    location: 'sadfas',
+    date: "Jan 28, 2022",
+    time: '12:00',
+    petsAllowed: false,
+    organizer: 'Admin'
+  },
+  {
+    id: 3,
+    category: 'a',
+    title: 'asdf',
+    description: 'asdf',
+    location: 'sadfas',
+    date: "Jan 28, 2022",
+    time: '12:00',
+    petsAllowed: false,
+    organizer: 'Admin'
+  },
+])
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
