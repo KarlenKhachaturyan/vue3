95bbb28244855ebe5756711f7fab437b443b146c
Karlen Khachaturyan <karlen.khachaturyan@gmail.com>
2024-11-14 15:00:02 +0400

    BaseTextArea

---
+++ vue-real-world/src/components/Base/BaseTextArea.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+    label: {
+        type: String,
+        default: '',
+    },
+    modelValue: {
+        type: [String, Number],
+        default: ''
+    }
+})
+
+</script>
+<!-- 
+ In order to add all the rest 
+ class/type/other attrs you can use v-bind='$attrs' 
+-->
+<template>
+    <label >{{ props.label }}</label>
+    <textarea 
+        v-bind="$attrs"
+        :value="modelValue" 
+        :placeholder="props.label"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
--- vue-real-world/src/views/FormView.vue
+++ vue-real-world/src/views/FormView.vue
@@ -1,89 +1,65 @@
 <script setup>
-import { reactive } from 'vue';
-import BaseInput from '@/components/Base/BaseInput.vue';
-import BaseSelect from '@/components/Base/BaseSelect.vue';
-import BaseCheckbox from '@/components/Base/BaseCheckbox.vue';
-import BaseRadioGroup from '@/components/Base/BaseRadioGroup.vue';
+import { reactive } from 'vue'
+import BaseInput from '@/components/Base/BaseInput.vue'
+import BaseSelect from '@/components/Base/BaseSelect.vue'
+import BaseCheckbox from '@/components/Base/BaseCheckbox.vue'
+import BaseRadioGroup from '@/components/Base/BaseRadioGroup.vue'
+import BaseTextArea from '@/components/Base/BaseTextArea.vue'
 
-const selectOptions = [
-    'a', 'b', 'c', 'd'
-]
+const selectOptions = ['a', 'b', 'c', 'd']
 
 const petOptions = [
-    {label: 'Cats', value: "cats"},
-    {label: 'Dogs', value: "dogs"},
+  { label: 'Cats', value: 'cats' },
+  { label: 'Dogs', value: 'dogs' }
 ]
 
 const event = reactive({
-    category: '',
-    title: '',
-    subtitle: '',
-    location: '',
-    pets: '',
-    extras: {
-        party: false,
-        music: false
-    }
+  category: '',
+  title: '',
+  subtitle: '',
+  location: '',
+  pets: '',
+  extras: {
+    party: false,
+    music: false
+  }
 })
 
 const sendForm = () => {
-    // here we will send the form data 
-    // using axios post....
+  // here we will send the form data
+  // using axios post....
 }
-
-
 </script>
 
 <template>
-    <h2>Create an event</h2>
-<form @submit.prevent="sendForm" style="text-align: left; display: flex; flex-direction: column; align-items: center;">
-    
-    <BaseSelect
-        :options="selectOptions"
-        v-model="event.category"
-        label="Select a Category"
-    />
+  <h2>Create an event</h2>
+  <form
+    @submit.prevent="sendForm"
+    style="text-align: left; display: flex; flex-direction: column; align-items: center"
+  >
+    <BaseTextArea label="Text area" v-model="event.category" maxLength="40" />
+    <BaseSelect :options="selectOptions" v-model="event.category" label="Select a Category" />
 
     <h4>Name and desccribe an event</h4>
 
-    <BaseInput 
-        label="Title"
-        v-model="event.title"
-    />
+    <BaseInput label="Title" v-model="event.title" />
 
-    <BaseInput 
-        label="Subtitle"
-        v-model="event.subtitle"
-    />
+    <BaseInput label="Subtitle" v-model="event.subtitle" />
 
     <h4>Where is your event</h4>
 
-    <BaseInput 
-        label="Location"
-        v-model="event.location"
-    />
+    <BaseInput label="Location" v-model="event.location" />
 
-    <h4> Are pets allowed</h4>
+    <h4>Are pets allowed</h4>
     <div>
-        <BaseRadioGroup
-            v-model="event.pets"
-            name="pets"
-            :options="petOptions"
-        />
+      <BaseRadioGroup v-model="event.pets" name="pets" :options="petOptions" />
     </div>
 
-    <h4>Extras  {{ event.extras }}</h4>
+    <h4>Extras {{ event.extras }}</h4>
 
-    <BaseCheckbox 
-        label="Party"
-        v-model="event.extras.party"
-        
-    />
-    <BaseCheckbox 
-        label="Music"
-        v-model="event.extras.music"
-    />
+    <BaseCheckbox label="Party" v-model="event.extras.party" />
+    <BaseCheckbox label="Music" v-model="event.extras.music" />
 
     <button type="submit">Submit</button>
-</form>
+  </form>
 </template>
